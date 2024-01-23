cd444641afe3df66c0082bc909287578ae091994
Nate Turman <nathant35047@stu.powayusd.com>
2024-01-23 22:01:56 +0000

    AddedWalls

--- assets/js/platformer2/Player.js
+++ assets/js/platformer2/Player.js
@@ -205,6 +205,12 @@ export class Player extends Character{
                 this.gravityEnabled = false;
                 this.setAnimation(this.directionKey); // set animation to direction
             }
+            if (this.collisionData.touchPoints.this.right) {
+                this.movement.right = false;
+            }
+            if (this.collisionData.touchPoints.this.left){
+                this.movement.left = false;
+            }
         }
         // Fall Off edge of Jump platform
         else if (this.movement.down === false) {
