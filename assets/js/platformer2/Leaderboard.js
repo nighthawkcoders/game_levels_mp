bcf5b03aa1d7ce47cb6dc280e01db66ca6624ab2
katiek27 <katiek5412@gmail.com>
2024-01-26 13:44:01 -0800

    leaderboard table added

--- assets/js/platformer2/Leaderboard.js
+++ assets/js/platformer2/Leaderboard.js
@@ -11,9 +11,51 @@ export class Leaderboard extends LocalStorage {
         this.loadAll();
     }
 
+    get leaderboardTable(){
+        // create table element
+        var t = document.createElement("leaderboardTable");
+        t.className = "leaderboard levels";
+        //create table header
+        var header = document.createElement("tr");
+        var th1 = document.createElement("th");
+        th1.innerText = "#";
+        header.append(th1);
+        var th2 = document.createElement("th");
+        th2.innerText = "Leaderboard Tag";
+        header.append(th2);
+        t.append(header);
+
+        // Create table rows/data
+        for(let i = 0, count = 1; i < GameEnv.levels.length; i++){
+            if (GameEnv.levels[i].passive) //skip passive levels
+                continue; 
+            // add level to table
+            var row = document.createElement("tr");
+            var td1 = document.createElement("td");
+            td1.innerText = String(count++); //human counter
+            row.append(td1);
+            // place level name in button   
+            var td2 = document.createElement("td");
+            td2.innerText = GameEnv.levels[i].tag;
+            row.append(td2);
+            // listen for row click
+            row.addEventListener("click",()=>{ // when player clicks on the row
+                //transition to selected level
+                GameControl.transitionToLevel(GameEnv.levels[i]); // resize event is triggered in transitionToLevel
+            })
+            // add level row to table
+            t.append(row);
+        }
+
+        return t; //returns <table> element
+    }
+
     static leaderboard(){
 
         console.log("function runs");
+
+        var scores = Leaderboard.leaderboardTable;
+        document.getElementById("leaderboard").append(scores);
 
         // Initiliaze Game settings controller 
         var settingsControl = new Leaderboard();
